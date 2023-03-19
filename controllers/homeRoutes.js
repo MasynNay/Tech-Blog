4b6088503425eb4df03df10c8031252d69a09168
Masyn N <masynnay33@gmail.com>
2023-03-18 20:46:01 -0600

    added more code
    added more code to project

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,26 +1,14 @@
 const router = require('express').Router();
-const { Project, User } = require('../models');
+const { User, Blog, Comment } = require('../models');
 const session = require('express-session');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    // Get all projects and JOIN with user data
-    const projectData = await Project.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
-
-    // Serialize data so the template can read it
-    const projects = projectData.map((project) => project.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
+    const blogData = await Blog.findAll()
+    const blog = blogData.map((blog) => blog.get({ plain: true }));
     res.render('homepage', { 
-      projects, 
+      blog, 
       logged_in: req.session.logged_in 
     });
   } catch (err) {
@@ -28,40 +16,41 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/project/:id', async (req, res) => {
+router.get('/blog/:id', async (req, res) => {
   try {
-    const projectData = await Project.findByPk(req.params.id, {
+    const blogData = await Blog.findByPk(req.params.id, {
       include: [
         {
-          model: User,
-          attributes: ['name'],
+          model: User, include: {
+            attributes: ['id', 'username'],
+          }
         },
+        {
+          model: Comment, include: {
+            model: User, 
+            attributes: ['id', 'username'],
+          }
+        }
       ],
     });
-
-    const project = projectData.get({ plain: true });
-
-    res.render('project', {
-      ...project,
+    const blog = blogData.get({ plain: true });
+    res.render('blog', {
+      ...blog,
       logged_in: req.session.logged_in
     });
   } catch (err) {
-    res.status(500).json(err);
+    res.status(400).json(err);
   }
 });
 
-// Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
+router.get('/dashboard', withAuth, async (req, res) => {
   try {
-    // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
-      include: [{ model: Project }],
+      include: [{ model: Blog }],
     });
-
     const user = userData.get({ plain: true });
-
-    res.render('profile', {
+    res.render('dashboard', {
       ...user,
       logged_in: true
     });
@@ -71,13 +60,20 @@ router.get('/profile', withAuth, async (req, res) => {
 });
 
 router.get('/login', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect('/dashboard');
     return;
   }
 
   res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+  res.render('signup');
 });
 
 module.exports = router;
--- public/js/comments.js
+++ public/js/comments.js
@@ -1,5 +1,19 @@
 const newCommentHandler = async (event) => {
   event.preventDefault();
-  const blog_id = document.querySelector('.new-comment').dataset.blog_id;
+  const blog_id = document.querySelector('#new-comment').dataset.blog_id;
   const comment_text = document.querySelector('#comment-text').value.trim();
-  
+  if (comment_text) {
+    const response = await fetch(`/api/comments`, {
+      method: 'POST',
+      body: JSON.stringify({ blog_id, comment_text }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (response.ok) {
+      document.location.reload();
+    } else {
+      alert('Cannot create comment');
+    }
+  }
+};
